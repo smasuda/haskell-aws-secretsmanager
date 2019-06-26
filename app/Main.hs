3e3db5054936ac44859b32842c96368751b854e7
smasuda <shunsuke.masuda@gmail.com>
2019-06-26 13:25:53 +0900

    parse json

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Data.Maybe                 (fromJust)
-import           Data.Text                  (Text)
-import qualified Network.AWS.SecretsManager as SM
-
 import           Control.Lens               (view)
 import           Control.Monad.Trans.AWS    (Credentials (Discover), Env,
                                              Region (Tokyo), newEnv, runAWST,
                                              runResourceT, send, within)
-
+import           Data.Aeson                 (Value, decode)
+import           Data.Aeson.Types           (Parser, parseMaybe, withObject,
+                                             (.:))
+import           Data.Maybe                 (fromJust)
+import           Data.String.Conversions    (cs)
+import           Data.Text                  (Text)
+import qualified Network.AWS.SecretsManager as SM
 
 main :: IO ()
-main = do
-  env <- newEnv Discover
-  ret <- getSecrets env "YOUR_SECRET_NAME" Tokyo -- Notice region 'Tokyo'. Change to yours.
-  print $ fromJust ret
+main = getSecretValueByKey "YOUR SECRET NAME" "SECRET KEY" >>= \x-> putStrLn (fromJust x)
 
+getSecretValueByKey::Text -> Text -> IO (Maybe String)
+getSecretValueByKey secretName secretKey = do
+    env <- newEnv Discover
+    ret <- getSecrets env secretName Tokyo -- Notice region 'Tokyo'. Change to yours.
+    return $ do
+        let decoded = ret >>= \x-> decode $ cs x ::Maybe Value
+        parseMaybe (parseSecrets secretKey) =<< decoded
 
-getSecrets :: Env -> Text -> Region-> IO (Maybe Text)
-getSecrets awsEnv secretArn region =
+parseSecrets::Text -> Value -> Parser String
+parseSecrets key = withObject "obj" $ \o-> o .: key
+
+getSecrets :: Env -> Text -> Region -> IO (Maybe Text)
+getSecrets awsEnv secretName region =
   runResourceT . runAWST awsEnv . within region $ do
-    resp <- send (SM.getSecretValue secretArn)
+    resp <- send (SM.getSecretValue secretName) -- could throw exception.
     return $ view SM.gsvrsSecretString resp
